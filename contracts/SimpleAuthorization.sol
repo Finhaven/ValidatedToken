cdf02457f74ba1b7634b98b685b18700d4da69c8
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-02-21 11:24:38 -0800

    Refactor SimpleAuthorization (#2)
    * Efficiency
    * Clearer naming

---
+++ contracts/SimpleAuthorization.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.19;
+
+import "eip820/contracts/EIP820Implementer.sol";
+import "giveth-common-contracts/contracts/Owned.sol";
+import "./TokenValidator.sol";
+
+// Reference Validator
+
+contract SimpleAuthorization is TokenValidator, Owned, EIP820Implementer {
+  mapping(address => bool) private authorizations;
+
+  function Whitelist() public {
+      setInterfaceImplementation("TokenValidator", this);
+  }
+
+  function check(address _token, address _address) public /* view */ returns (uint8 resultCode) {
+      return authorizations[_address] ? 1 : 0;
+  }
+
+  function check(
+      address _token,
+      address _from,
+      address _to,
+      uint256 _amount
+  ) public /* view */ returns (uint8 resultCode) {
+      return (authorizations[_from] && authorizations[_to]) ? 1 : 0;
+  }
+
+  function setAuthorized(address _address, bool _status) public onlyOwner {
+      authorizations[_address] = _status;
+  }
+}
