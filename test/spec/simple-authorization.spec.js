6227ee9801216295542f76ed8544b21dd66d282d
User <naumenko.gs@gmail.com>
2018-03-30 11:17:47 -0700

    wrapper to handle overloading in truffle test

--- test/spec/simple-authorization.spec.js
+++ test/spec/simple-authorization.spec.js
@@ -1,4 +1,5 @@
-const SimpleAuthorization = artifacts.require('SimpleAuthorization');
+// Because truffle does not support overloading
+const SimpleAuthorization = artifacts.require('ExtendedSimpleAuthorization');
 
 contract('SimpleAuthorization', (accounts) => {
   let simpleAuthorization;
@@ -13,29 +14,28 @@ contract('SimpleAuthorization', (accounts) => {
   });
 
 
-  // 4-parameter checks are used because truffle does not support overloading and can't find check(A, B)
   it('ownder should be able to authorize and check auth', async () => {
       targetAccount = accounts[1];
-      authorizedBefore = await simpleAuthorization.check.call(0x0, targetAccount, targetAccount, 0);
+      authorizedBefore = await simpleAuthorization.check2.call(0x0, targetAccount);
       assert(authorizedBefore == 0, 'should not be already authorized');
       await simpleAuthorization.setAuthorized(targetAccount, true);
-      authorizedAfter = await simpleAuthorization.check.call(0x0, targetAccount, targetAccount, 0);
+      authorizedAfter = await simpleAuthorization.check2.call(0x0, targetAccount);
       assert(authorizedAfter == 1, 'should become authorized');
       await simpleAuthorization.setAuthorized(targetAccount, false);
-      authorizedAfter = await simpleAuthorization.check.call(0x0, targetAccount, targetAccount, 0);
+      authorizedAfter = await simpleAuthorization.check2.call(0x0, targetAccount);
       assert(authorizedAfter == 0, 'should become unauthorized');
   });
 
   it('transfer should pass validation only when both accounts are authorized', async () => {
       sender = accounts[1];
       receiver = accounts[2];
-      authorizedBefore = await simpleAuthorization.check.call(0x0, sender, receiver, 0);
+      authorizedBefore = await simpleAuthorization.check4.call(0x0, sender, receiver, 0);
       assert(authorizedBefore == 0, 'should not be already authorized');
       await simpleAuthorization.setAuthorized(receiver, true);
-      authorizedAfter = await simpleAuthorization.check.call(0x0, sender, receiver, 0);
+      authorizedAfter = await simpleAuthorization.check4.call(0x0, sender, receiver, 0);
       assert(authorizedAfter == 0, 'should still be unauthorized');
       await simpleAuthorization.setAuthorized(sender, true);
-      authorizedAfter = await simpleAuthorization.check.call(0x0, sender, receiver, 0);
+      authorizedAfter = await simpleAuthorization.check4.call(0x0, sender, receiver, 0);
       assert(authorizedAfter == 1, 'should become authorized');
   });
 });
