6db9e5446dbdb0059f95fe102e291b86d5574517
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-04-11 23:30:29 -0700

    External

--- contracts/TokenValidator.sol
+++ contracts/TokenValidator.sol
@@ -4,12 +4,12 @@ interface TokenValidator {
   function check(
     address _token,
     address _user
-  ) public returns(byte result);
+  ) external returns(byte result);
 
   function check(
     address _token,
     address _from,
     address _to,
     uint256 _amount
-  ) public returns (byte result);
+  ) external returns (byte result);
 }
